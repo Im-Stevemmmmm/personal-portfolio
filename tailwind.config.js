9d8d803b340310029373f124a742da18d3d1da30
Esteban Garcia <estebangarcia2121@gmail.com>
2021-05-12 12:32:41 -0700

    Added react-spring and typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      darkGray: "#111111"
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
